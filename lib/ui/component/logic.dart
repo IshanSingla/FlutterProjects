6254bc24f569380d549080304c8f6ee1784082ec
Ishan Singla <79676239+IshanSingla@users.noreply.github.com>
2022-03-20 16:43:04 +0530

    Update logic.dart

--- lib/ui/component/logic.dart
+++ lib/ui/component/logic.dart
@@ -2,19 +2,28 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 
-Map<String, String> headers = {
-      HttpHeaders.contentTypeHeader: 'application/json',
-};
-var response = http.get(Uri.https(
-  'www.induced.me',
-  '/api/moneyin',
-      {
-        'userid': 1234,
-        'amount': 20,
-      },);
+class APIService {
+  APIService._instantiate();
 
-if (response.statusCode == 200) {
-    Map<String, dynamic> data = json.decode(response.body);
+  static final APIService instance = APIService._instantiate();
+
+
+  Future<List<Video>> search({required String word}) async {
+    Map<String, String> headers = {
+      HttpHeaders.contentTypeHeader: 'application/json',
+    };
+    var response = http.get(Uri.https(
+        'www.induced.me',
+        '/api/moneyin',
+        {
+          'userid': 1234,
+          'amount': 20,
+        }),);
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = json.decode(response.body);
+    }
+  }
 }
 
 
