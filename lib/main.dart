773e212cde7a373ce311f7de875787e8e181f2f7
Ishan Singla <79676239+IshanSingla@users.noreply.github.com>
2022-03-20 19:27:51 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    dynamic futureAlbum = fetchAlbum();
+    print(fetchAlbum());
   }
     _MyAppState(){
       print(futureAlbum);
