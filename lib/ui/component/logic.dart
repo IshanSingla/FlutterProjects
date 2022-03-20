51e298acaed33d95d2877cb37978ff2730d09294
Ishan Singla <79676239+IshanSingla@users.noreply.github.com>
2022-03-20 19:18:54 +0530

    Update logic.dart

--- lib/ui/component/logic.dart
+++ lib/ui/component/logic.dart
@@ -17,3 +17,41 @@ dynamic fetchAlbum() async {
   }
 }
 
+dynamic moneyout() async {
+  final response = await http
+      .get(Uri.parse('https://www.induced.me/api/moneyout?userid=123&amount=10'));
+
+  if (response.statusCode == 200) {
+    print(response.body);
+    return json.decode(response.body);
+
+  } else {
+    return {"stats":"Page Not Found"};
+  }
+}
+
+dynamic moneycheck() async {
+  final response = await http
+      .get(Uri.parse('https://www.induced.me/api/moneycheck?userid=123'));
+
+  if (response.statusCode == 200) {
+    print(response.body);
+    return json.decode(response.body);
+
+  } else {
+    return {"stats":"Page Not Found"};
+  }
+}
+
+dynamic moneytrans() async {
+  final response = await http
+      .get(Uri.parse('https://www.induced.me/api/moneytrans?userid=123'));
+
+  if (response.statusCode == 200) {
+    print(response.body);
+    return json.decode(response.body);
+
+  } else {
+    return {"stats":"Page Not Found"};
+  }
+}
