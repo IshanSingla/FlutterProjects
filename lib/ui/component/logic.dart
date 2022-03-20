3f422466b48b4864393e356ed7594e5a2de28e60
Ishan Singla <79676239+IshanSingla@users.noreply.github.com>
2022-03-20 15:47:26 +0530

    Update logic.dart

--- lib/ui/component/logic.dart
+++ lib/ui/component/logic.dart
@@ -1,3 +1,7 @@
-class Data{
 
-}
+import 'package:http/http.dart' as http;
+
+var url = Uri.parse('https://www.induced.me/api/moneyin');
+var response = await http.post(url, body: {'userid': 1233, 'ammount': 20});
+print('Response status: ${response.statusCode}');
+print('Response body: ${response.body}');
